d260ddd3ba47c5c330c1c5ec84fe1734597a0b27
ziahamza <ziahamza2007@gmail.com>
2012-07-23 17:29:21 +0800

    knockout bindings

---
+++ Wirecraft.Web/Scripts/script.js
@@ -0,0 +1,22 @@
+﻿var utils = {
+    convertToViewModel: function(data) {
+        var viewModel = {};
+        for (var table in data) {
+            var observableTable = ko.observableArray(data[table].map(function (e) {
+                var observableEntity = {};
+                for (var entity in e) {
+                    if (Object.prototype.toString.call(e[entity]) === '[object Array]') {
+                        observableEntity[entity] = ko.observableArray([entity].map(ko.observable));
+                    }
+                    else {
+                        observableEntity[entity] = ko.observable(e[entity]);
+                    }
+                }
+                return observableEntity;
+            }));
+
+            viewModel[table] = observableTable;
+        }
+        return viewModel;
+    }
+};
